7de032b460b6b4a4ea119b32098ac314f8c89c1f
Winfried Baumann <codewing@web.de>
2024-08-27 20:54:34 +0200

    window done

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use winit::{
+    event::{ElementState, Event, KeyEvent, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+    run().await;
+}
+
+async fn run() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = wgpu_playground::app_state::AppState::new(&window).await;
+
+    let _ = event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == state.window.id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                state: ElementState::Pressed,
+                                physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => control_flow.exit(),
+                    WindowEvent::Resized(new_size) => state.resize(*new_size),
+                    WindowEvent::RedrawRequested => {
+                        state.update();
+                        match state.render() {
+                            Ok(_) => {}
+                            // Reconfigure the surface if lost
+                            Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                            // The system is out of memory, we should probably quit
+                            Err(wgpu::SurfaceError::OutOfMemory) => control_flow.exit(),
+                            // All other errors (Outdated, Timeout) should be resolved by the next frame
+                            Err(e) => eprintln!("{:?}", e),
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        // ... at the end of the WindowEvent block
+        Event::AboutToWait => {
+            // RedrawRequested will only trigger once unless we manually
+            // request it.
+            state.window().request_redraw();
+        }
+        // ...
+        _ => {}
+    });
+}
